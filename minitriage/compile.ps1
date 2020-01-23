4dd0e7877cc6759c5f3cefbca2a3c3ebbfee4aef
James Dickson <wallparse@gmail.com>
2020-01-23 15:10:19 +0100

    Added simple powershell command to compile project

---
+++ minitriage/compile.ps1
@@ -0,0 +1,38 @@
+# James simpla kompilera-verktyg-kommando...
+$dict = New-Object 'system.collections.generic.dictionary[[string],[string]]'
+$dict.Add("CompilerVersion","v4.0")
+
+
+[string[]]$references=@("System.dll",
+                        "System.Xml.dll",
+                        "System.Core.dll",
+                        "System.Text.RegularExpressions.dll",
+                        "System.IO.Compression.ZipFile.dll",
+                        "System.IO.Compression.FileSystem.dll",
+                        "System.Diagnostics.Process.dll",
+                        "netstandard.dll",
+                        "System.IO.Compression.dll")
+
+$strWorkDirectory = get-location
+$codeprovider = new-object Microsoft.CSharp.CSharpCodeProvider $dict
+
+$icc = $codeprovider.CreateCompiler()
+$opt = new-object System.CodeDom.Compiler.CompilerParameters
+$opt.ReferencedAssemblies.AddRange($references)
+$opt.GenerateExecutable = $true
+$opt.MainClass = "minitriage.Program"
+$opt.OutputAssembly = "$strWorkDirectory\minitriage.exe"
+
+$strSources = Get-ChildItem $strWorkDirectory -Include "*.cs" -recurse;
+$strFullPaths = ($strSources | % { $_.FullName; });
+
+write-host "[+] Compiling executable ..."
+$result = $icc.CompileAssemblyFromFileBatch($opt, $strFullPaths)
+
+write-host "[+] Done with nr of errors:"  $result.Errors.Count
+
+if($result.Errors.Count -gt 0)
+{
+    write-host $result.Errors
+}
+
